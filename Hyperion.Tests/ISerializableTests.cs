6e184070713b48528da1688cac427ca66dd5bd28
ravengerUA <yurisikorsky@gmail.com>
2017-03-25 12:21:54 +0200

    restored support of ISerialization

--- Hyperion.Tests/ISerializableTests.cs
+++ Hyperion.Tests/ISerializableTests.cs
@@ -7,9 +7,86 @@
 // -----------------------------------------------------------------------
 #endregion
 
+#if SERIALIZATION
+using System;
+using System.Runtime.Serialization;
+using Xunit;
+
 namespace Hyperion.Tests
 {
     public class ISerializableTests : TestBase
     {
+        public class NonSerializedTest
+        {
+            public int Field1;
+
+            [NonSerialized]
+            public string Field2;
+        }
+
+        public class NotSerializableEvent : ISerializable
+        {
+            public NotSerializableEvent(bool foo) { }
+
+            public void GetObjectData(SerializationInfo info, StreamingContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class Person : ISerializable
+        {
+            public Person(string firstName, string lastName)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+            }
+
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public void GetObjectData(SerializationInfo info, StreamingContext streamingContext)
+            {
+                info.AddValue("FirstName", FirstName);
+                info.AddValue("LastName", LastName);
+            }
+        }
+
+        [Fact]
+        public void CanIgnoreFieldsWithNonSerializedAttribute()
+        {
+            var expected = new NonSerializedTest() {
+                Field1 = 235,
+                Field2 = "non serialized text"
+            };
+
+            Serialize(expected);
+            Reset();
+            var actual = Deserialize<NonSerializedTest>();
+            Assert.Equal(expected.Field1, actual.Field1);
+            Assert.Null(actual.Field2);
+        }
+
+        [Fact(Skip="Not implemented yet")]
+        public void CanSerializeClassesWithISerializable()
+        {
+            var expected = new Person("Scott", "Hanselman");
+
+            Serialize(expected);
+            Reset();
+            var actual = Deserialize<Person>();
+            Assert.Equal(expected.FirstName, actual.FirstName);
+            Assert.Equal(expected.LastName, actual.LastName);
+        }
+
+        [Fact]
+        public void ShouldThrowOnNonSerializableEvent()
+        {
+            var expected = new NotSerializableEvent(true);
+
+            Assert.Throws<NotImplementedException>(() => Serialize(expected));
+        }
     }
 }
+#endif
