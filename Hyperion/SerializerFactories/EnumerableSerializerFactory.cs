850fe1505b20debe5c5baa06311cc0e96cb2e32d
joey-becker <26005405+joey-becker@users.noreply.github.com>
2018-01-08 00:41:48 -0600

    EnumerableSerializeFactory fixes (#81)
    * EnumerableSerializeFactory fixes
    
    * Update EnumerableSerializerFactory. Move null checks outside ObjectReader, allow private constructors.
    
    * Update EnumerableSerializerFactor to use Add method if exists, then IEnumerable<T> ctor.

--- Hyperion/SerializerFactories/EnumerableSerializerFactory.cs
+++ Hyperion/SerializerFactories/EnumerableSerializerFactory.cs
@@ -13,6 +13,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Linq.Expressions;
 using Hyperion.Extensions;
 using Hyperion.ValueSerializers;
 
@@ -22,35 +23,46 @@ namespace Hyperion.SerializerFactories
     {
         public override bool CanSerialize(Serializer serializer, Type type)
         {
-            //TODO: check for constructor with IEnumerable<T> param
+            if (!typeof(IEnumerable).IsAssignableFrom(type))
+                return false;
+
+            // Stack<T> has IEnumerable<T> constructor, but reverses order of the stack, so can't be used.
+            if (type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == typeof(Stack<>))
+                return false;
 
             var countProperty = type.GetTypeInfo().GetProperty("Count");
             if (countProperty == null || countProperty.PropertyType != typeof(int))
                 return false;
 
-            if (!type.GetTypeInfo().GetMethods().Any(IsAddMethod))
-                return false;
-
-            var isGenericEnumerable = GetEnumerableType(type) != null;
-            if (isGenericEnumerable)
+            var hasEnumerableConstructor = GetEnumerableConstructor(type) != null;
+            if (hasEnumerableConstructor)
                 return true;
 
-            if (typeof(ICollection).GetTypeInfo().IsAssignableFrom(type))
+            if (!HasParameterlessConstructor(type))
+                return false;
+            
+            if (type.GetTypeInfo().GetMethods(BindingFlagsEx.All).Any(IsAddMethod))
                 return true;
 
             return false;
         }
-
         private static bool IsAddMethod(MethodInfo methodInfo) => 
-            (methodInfo.Name == "AddRange" || methodInfo.Name == "Add")
+            methodInfo.Name == "Add"
             && (methodInfo.ReturnType == typeof(void) || methodInfo.ReturnType == typeof(bool)) // sets return bool on Add
             && !methodInfo.IsStatic
             && HasValidParameters(methodInfo);
-
+        
         private static bool HasValidParameters(MethodInfo methodInfo)
         {
             var parameters = methodInfo.GetParameters();
             return parameters.Length == 1;
+        }
+
+        private static bool HasParameterlessConstructor(Type type)
+        {
+            return type.GetTypeInfo()
+                .GetConstructors(BindingFlagsEx.All)
+                .Any(ctor => !ctor.GetParameters().Any());
         }
 
         public override bool CanDeserialize(Serializer serializer, Type type)
@@ -68,58 +80,104 @@ namespace Hyperion.SerializerFactories
                 .FirstOrDefault();
         }
 
+        private static ConstructorInfo GetEnumerableConstructor(Type type)
+        {
+            var enumerableType = GetEnumerableType(type);
+            var iEnumerableType = typeof(IEnumerable<>).MakeGenericType(enumerableType);
+            return enumerableType != null
+                ? type.GetTypeInfo()
+                    .GetConstructors(BindingFlagsEx.All)
+                    .Where(ctor => HasSingleParameterOfType(ctor, iEnumerableType))
+                    .FirstOrDefault()
+                : null;
+        }
+
+        private static bool HasSingleParameterOfType(MethodBase methodInfo, Type type)
+        {
+            var parameters = methodInfo.GetParameters();
+            return parameters.Length == 1 && parameters[0].ParameterType == type;
+        }
+
+        private static Func<object, object> CompileCtorToDelegate(ConstructorInfo ctor, Type argType)
+        {
+            var arg = Expression.Parameter(typeof(object));
+            var castArg = Expression.Convert(arg, argType);
+            var call = Expression.New(ctor, new Expression[] { castArg });
+            var castRes = Expression.Convert(call, typeof(object));
+            var lambda = Expression.Lambda<Func<object, object>>(castRes, arg);
+            var compiled = lambda.Compile();
+            return compiled;
+        }
+
+        private static Action<object, object> CompileMethodToDelegate(MethodInfo method, Type instanceType, Type argType)
+        {
+            var instance = Expression.Parameter(typeof(object));
+            var arg = Expression.Parameter(typeof(object));
+            var castInstance = Expression.Convert(instance, instanceType);
+            var castArg = Expression.Convert(arg, argType);
+            var call = Expression.Call(castInstance, method, new Expression[] { castArg });
+            var lambda = Expression.Lambda<Action<object, object>>(call, instance, arg);
+            var compiled = lambda.Compile();
+            return compiled;
+        }
+
         public override ValueSerializer BuildSerializer(Serializer serializer, Type type,
             ConcurrentDictionary<Type, ValueSerializer> typeMapping)
         {
             var x = new ObjectSerializer(type);
             typeMapping.TryAdd(type, x);
+
             var preserveObjectReferences = serializer.Options.PreserveObjectReferences;
 
             var elementType = GetEnumerableType(type) ?? typeof(object);
             var elementSerializer = serializer.GetSerializerByType(elementType);
 
             var countProperty = type.GetTypeInfo().GetProperty("Count");
-            var addRange = type.GetTypeInfo().GetMethod("AddRange");
-            var add = type.GetTypeInfo().GetMethod("Add");
+
+            var addMethod = type.GetTypeInfo().GetMethod("Add", BindingFlagsEx.All);
+            var enumerableConstructor = GetEnumerableConstructor(type);
 
             Func<object, int> countGetter = o => (int)countProperty.GetValue(o);
-
-
-            ObjectReader reader = (stream, session) =>
+            ObjectReader reader = null;
+            if (addMethod != null)
             {
-                var instance = Activator.CreateInstance(type);
-                if (preserveObjectReferences)
+                var add = CompileMethodToDelegate(addMethod, type, elementType);
+                reader = (stream, session) =>
                 {
-                    session.TrackDeserializedObject(instance);
-                }
-
-                var count = stream.ReadInt32(session);
-
-                if (addRange != null)
+                    var instance = Activator.CreateInstance(type, true);
+                    if (preserveObjectReferences)
+                    {
+                        session.TrackDeserializedObject(instance);
+                    }
+                    var count = stream.ReadInt32(session);
+                    for (var i = 0; i < count; i++)
+                    {
+                        var value = stream.ReadObject(session);
+                        add(instance, value);
+                    }
+                    return instance;
+                };
+            }
+            else if (enumerableConstructor != null)
+            {
+                var construct = CompileCtorToDelegate(enumerableConstructor, elementType.MakeArrayType());
+                reader = (stream, session) =>
                 {
+                    var count = stream.ReadInt32(session);
                     var items = Array.CreateInstance(elementType, count);
                     for (var i = 0; i < count; i++)
                     {
                         var value = stream.ReadObject(session);
                         items.SetValue(value, i);
                     }
-                    //HACK: this needs to be fixed, codegenerated or whatever
-
-                    addRange.Invoke(instance, new object[] { items });
-                    return instance;
-                }
-                if (add != null)
-                {
-                    for (var i = 0; i < count; i++)
+                    var instance = construct(items);
+                    if (preserveObjectReferences)
                     {
-                        var value = stream.ReadObject(session);
-                        add.Invoke(instance, new[] { value });
+                        session.TrackDeserializedObject(instance);
                     }
-                }
-
-
-                return instance;
-            };
+                    return instance;
+                };
+            }
 
             ObjectWriter writer = (stream, o, session) =>
             {
