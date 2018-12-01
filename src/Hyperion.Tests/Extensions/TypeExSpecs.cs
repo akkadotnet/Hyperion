01892136a91659c24fcc12cfb76338a77a7a1244
Aaron Stannard <aaron@petabridge.com>
2018-11-30 20:05:22 -0600

    added TypeEx specs

---
+++ src/Hyperion.Tests/Extensions/TypeExSpecs.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="TypeExSpecs.cs" company="Akka.NET Team">
+//      Copyright (C) 2016-2018 Akka.NET Team <https://github.com/akkadotnet>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Hyperion.Extensions;
+using Xunit;
+
+namespace Hyperion.Tests.Extensions
+{
+    public class TypeExSpecs
+    {
+        [Fact]
+        public void GetShortAssemblyQualifiedNameShouldStripAssemblyInfo()
+        {
+            var immutableInterlockedType = typeof(ImmutableInterlocked);
+            var shortName = immutableInterlockedType.GetShortAssemblyQualifiedName();
+
+            // shouldn't have any version data
+            shortName.Should().Be("System.Collections.Immutable.ImmutableInterlocked, System.Collections.Immutable");
+        }
+
+        //[Fact]
+        //public void GetShortAssemblyQualifiedNameShouldStripAssemblyInfoForGenericTypes()
+        //{
+        //    var immutableArrayType = typeof(ImmutableArray<int>);
+        //    var shortName = immutableArrayType.GetShortAssemblyQualifiedName();
+
+        //    // shouldn't have any version data
+        //    shortName.Should().Be("System.Collections.Immutable.ImmutableInterlocked, System.Collections.Immutable");
+        //}
+    }
+}
