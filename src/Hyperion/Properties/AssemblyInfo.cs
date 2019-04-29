a85899c42fde0dbe6fad40efbacbd5db588add58
Alexander Jesner <alexander@jesner.eu>
2019-04-29 12:31:52 +0200

    Add a strong name signing key and activate signing

--- src/Hyperion/Properties/AssemblyInfo.cs
+++ src/Hyperion/Properties/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Security;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("7af8d2b6-9f1f-4a1c-8673-48e533108385")]
-[assembly: InternalsVisibleTo("Hyperion.Tests")]
+[assembly: InternalsVisibleTo("Hyperion.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d1b02d7d7ee028be7b02edc08abd7f95760f1cc3fee6c48936b9febf4d6f76da586b8932c3a87c5912ddc66028a1841063fa1d4d1dc2814b58deb14cce7d18ffe3d8475bc2fcba11c480f96bcc41355c8f0131c6c3b2c8005ae5744e29498875a487e0f8c977b87236a596680ace77082889305e1e9f2907147add28536c99d8")]
 
 #if UNSAFE
 [assembly: AllowPartiallyTrustedCallers]
