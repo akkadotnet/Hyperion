d82a50ac9ea2b705ee0614661504372ae9f90b0c
deniz_irgin@yahoo.com <deniz_irgin@yahoo.com>
2019-05-28 14:07:18 +0300

    Add a binary file generator for all .NET frameworks

---
+++ src/Hyperion.Tests/Generator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Text.RegularExpressions;
+
+namespace Hyperion.Tests.Generator
+{
+    internal static class Program
+    {
+        private static readonly Serializer Serializer = new Serializer();
+        private static readonly string FullFrameworkName = Assembly
+            .GetEntryAssembly()?
+            .GetCustomAttribute<TargetFrameworkAttribute>()?
+            .FrameworkName;
+
+        private static readonly string FrameworkName = Regex.Replace(FullFrameworkName, "[^\\w\\._]", string.Empty);
+
+        private static void Main(string[] args)
+        {
+            if (args.Length != 1 || args[0] != "generate")
+            {
+                return;
+            }
+
+            string outputPath = args.Length == 2 ? args[1] : CrossFrameworkInitializer.DefaultOutputPath;
+
+            if (!Directory.Exists(outputPath))
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+
+            CrossFrameworkClass crossFrameworkClass = CrossFrameworkInitializer.Init();
+
+            string fileName = $"test_file_{FrameworkName.ToLowerInvariant()}.tf";
+            string fullPath = Path.Combine(outputPath, fileName);
+
+            using (var fileStream = new FileStream(fullPath, FileMode.Create))
+            {
+                Serializer.Serialize(crossFrameworkClass, fileStream);
+            }
+        }
+    }
+}
