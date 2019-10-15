034ba5c243b11ac62e7d6e5a0e1c4501143444af
Deniz İrgin <denizirgin@gmail.com>
2019-10-15 20:38:23 +0300

    WIP Communicate between framework mscorlib and core framework with Unit Tests (#116)
    *  Communicate between mscorlib and System.Private.CoreLib
    
    * Strict string match
    
    * Add a binary file generator for all .NET frameworks
    
    * Add generated serialized files to project as suggestted in https://github.com/akkadotnet/Hyperion/pull/112#issuecomment-486078678_
    
    * Add unit tests to test cross framework serialization and deserialization
    
    * FullFramework or CoreNetFramework runtime check
    
    * Using Constants instead
    
    * Downgrade FSharp.Core 4.7.0 to 4.6.2 because 4.7.0 is not compatible with netstandard1.6
    
    * Add generated serialized file for .NET Core 3.0  to project
    
    * Add netcoreapp2.2 support to Hyperion.Tests
    Update Windows vm image to windows-2019
    
    * Remove netcoreapp2.2 from Hyperion.Tests

--- src/Hyperion/Extensions/TypeEx.cs
+++ src/Hyperion/Extensions/TypeEx.cs
@@ -118,11 +118,23 @@ namespace Hyperion.Extensions
             return TypeNameLookup.GetOrAdd(byteArr, b =>
             {
                 var shortName = StringEx.FromUtf8Bytes(b.Bytes, 0, b.Bytes.Length);
+#if NET45
+                if (shortName.Contains("System.Private.CoreLib,%core%"))
+                {
+                    shortName = shortName.Replace("System.Private.CoreLib,%core%", "mscorlib,%core%");
+                }
+#endif
+#if NETSTANDARD
+                if (shortName.Contains("mscorlib,%core%"))
+                {
+                    shortName = shortName.Replace("mscorlib,%core%", "System.Private.CoreLib,%core%");
+                }
+#endif
+
                 var typename = ToQualifiedAssemblyName(shortName);
                 return Type.GetType(typename, true);
             });
         }
-
         public static Type GetTypeFromManifestFull(Stream stream, DeserializerSession session)
         {
             var type = GetTypeFromManifestName(stream, session);
@@ -195,7 +207,7 @@ namespace Hyperion.Extensions
 
             throw new NotSupportedException();
         }
-
+        private static readonly string CoreAssemblyQualifiedName = 1.GetType().AssemblyQualifiedName;
         private static readonly string CoreAssemblyName = GetCoreAssemblyName();
 
         private static readonly Regex cleanAssemblyVersionRegex = new Regex(
@@ -204,8 +216,7 @@ namespace Hyperion.Extensions
 
         private static string GetCoreAssemblyName()
         {
-            var name = 1.GetType().AssemblyQualifiedName;
-            var part = name.Substring( name.IndexOf(", Version", StringComparison.Ordinal));
+            var part = CoreAssemblyQualifiedName.Substring(CoreAssemblyQualifiedName.IndexOf(", Version", StringComparison.Ordinal));
             return part;
         }
 
